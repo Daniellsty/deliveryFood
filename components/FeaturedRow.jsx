aa7cbb564db4b5bb4a755996389384b2247bec01
danyallsty <danyallsty04@gmail.com>
2024-09-01 12:59:47 +0330

    featured row added to home

--- components/FeaturedRow.jsx
+++ components/FeaturedRow.jsx
@@ -1,15 +1,14 @@
-import ResturantCard from './ResturantCard';
-import { category } from "@/constants";
+import ResturantCard from "./ResturantCard";
 import { themeColors } from "@/theme/themeColor";
 import { View, Text, TouchableOpacity, ScrollView } from "react-native";
 
-const FeaturedRow = ({ title }) => {
+const FeaturedRow = ({ title, description, restaurants }) => {
   return (
     <View>
       <View className="flex-row justify-between items-center px-4 flex-1">
         <View>
           <Text className="font-bold text-lg">{title}</Text>
-          <Text className="text-gray-500 text-xs">{title}</Text>
+          <Text className="text-gray-500 text-xs">{description}</Text>
         </View>
         <TouchableOpacity>
           <Text style={{ color: themeColors.text }} className="font-semibold">
@@ -17,22 +16,16 @@ const FeaturedRow = ({ title }) => {
           </Text>
         </TouchableOpacity>
       </View>
-        <ScrollView 
+      <ScrollView
         horizontal
         showsHorizontalScrollIndicator={false}
-        contentContainerStyle={{paddingHorizontal:15 }}
-        className="overflow-visible py-5">
-            {
-                category.map((item,index)=>{
-                    return(
-                        <ResturantCard  
-                        key={index}  
-                        item={item}/>
-                    )
-                })
-            }
-
-        </ScrollView>
+        contentContainerStyle={{ paddingHorizontal: 15 }}
+        className="overflow-visible py-5"
+      >
+        {restaurants?.map((item, index) => {
+          return <ResturantCard key={index} item={item} />;
+        })}
+      </ScrollView>
     </View>
   );
 };
