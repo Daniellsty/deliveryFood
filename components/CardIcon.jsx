279835f26eae9f8acce4fdf8e47c61ce98430e66
danyallsty <danyallsty04@gmail.com>
2024-09-01 12:59:18 +0330

    card icon to cart screen added

--- components/CardIcon.jsx
+++ components/CardIcon.jsx
@@ -1,10 +1,16 @@
 import { View, Text, TouchableOpacity } from "react-native";
-import React from "react";
 import { themeColors } from "@/theme/themeColor";
 import { useNavigation } from "expo-router";
+import { useSelector } from "react-redux";
+import { selectCartItems, selectedTotalCartPrice } from "@/redux/cartSlice";
 
 const CardIcon = () => {
   const navigation = useNavigation();
+  const cartItems = useSelector(selectCartItems);
+  const cartTotal = useSelector(selectedTotalCartPrice)
+
+
+  if(!cartItems.length) return;
 
   return (
     <View className="absolute bottom-5 w-full z-50">
@@ -16,13 +22,13 @@ const CardIcon = () => {
         <View
         style={{backgroundColor:'rgba(255,255,255,0.3)'}}
         className="p-2 px-4 rounded-full">
-          <Text className="font-extrabold text-white text-lg">3</Text>
+          <Text className="font-extrabold text-white text-lg">{cartItems.length}</Text>
         </View>
         <Text className="flex-1 text-center font-extrabold text-white text-lg">
         view Cart
         </Text>
         <Text className="font-extrabold text-white text-lg">
-            ${23}
+            ${cartTotal}
         </Text>
       </TouchableOpacity>
     </View>
