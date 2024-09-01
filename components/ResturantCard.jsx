7008b9245aae663a595ae1305dd66444c13045a1
danyallsty <danyallsty04@gmail.com>
2024-09-01 13:00:05 +0330

    resutrant card of resturant route

--- components/ResturantCard.jsx
+++ components/ResturantCard.jsx
@@ -3,9 +3,11 @@ import Feather from "@expo/vector-icons/Feather";
 import { themeColors } from "@/theme/themeColor";
 import AntDesign from "@expo/vector-icons/AntDesign";
 import { useNavigation } from "expo-router";
+import { urlFor } from "@/SanityClient";
 
 const ResturantCard = ({ item }) => {
   const navigatation = useNavigation();
+
 
   return (
     <TouchableWithoutFeedback
@@ -18,7 +20,7 @@ const ResturantCard = ({ item }) => {
         }}
         className="mr-6 bg-white rounded-3xl shadow-xl mb-3 "
       >
-        <Image className="h-36 w-64 rounded-t-3xl" source={item.image} />
+        <Image className="h-36 w-64 rounded-t-3xl" source={{uri:urlFor(item.image).url()}} />
         <View className="px-3 pb-4 space-y-2">
           <Text className="text-lg font-bold pt-2">{item.name}</Text>
           <View className="flex-row items-center space-x-1">
@@ -27,14 +29,14 @@ const ResturantCard = ({ item }) => {
               <Text className="text-green-700">{item?.stars}</Text>
               <Text className="text-gray-700">
                 ({item?.reviews} review) .
-                <Text className="font-semibold">{item?.category}</Text>
+                <Text className="font-semibold">{item?.type?.name}</Text>
               </Text>
             </Text>
           </View>
 
           <View className="flex-row items-center space-x-1">
             <Feather name="map-pin" size={15} color="gray" />
-            <Text className="text-gray-700 text-xs">
+            <Text className="text-gray-700 text-xs truncate flex-1">
               Nearby. {item?.address}
             </Text>
           </View>
