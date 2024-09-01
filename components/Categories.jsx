f728f5e29c0162c9ca28c6e8b4f89c9299d40e53
danyallsty <danyallsty04@gmail.com>
2024-09-01 12:56:15 +0330

    categories component to top of home routed

--- components/Categories.jsx
+++ components/Categories.jsx
@@ -1,10 +1,21 @@
 import { View, Text, ScrollView, TouchableOpacity, Image } from "react-native";
-import { category } from "../constants/index";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+
+import {getCategories} from '../api';
+import { urlFor } from "@/SanityClient";
 
 const Categories = () => {
 
   const [activeCategory,setActiveCategory] = useState(null);
+  const [categories,setCategories] = useState([])
+
+  useEffect(()=>{
+    getCategories().then(data=>{
+ 
+      setCategories(data);
+      
+    })
+  },[])
 
   return (
     <View>
@@ -16,17 +27,17 @@ const Categories = () => {
           paddingHorizontal: 15,
         }}
       >
-        {category.map((item, index) => {
-          const isActive = item.id == activeCategory;
+        {categories?.map((item, index) => {
+          const isActive = item._id == activeCategory;
           const btnClass = isActive? 'bg-gray-600' : 'bg-gray-200';
           const textClass = isActive? 'font-semibold text-gray-800' : 'text-gray-500';
 
           return (
             <View key={index} className="flex justify-center items-center mr-6">
               <TouchableOpacity 
-              onPress={()=> setActiveCategory(item.id)}
+              onPress={()=> setActiveCategory(item._id)}
               className={"bg-gray-200 p-2 rounded-full shadow " +btnClass}>
-                <Image source={item.image} style={{width:45,height:45}}/>
+                <Image source={{uri:urlFor(item.image).url()}} style={{width:45,height:45}}/>
               </TouchableOpacity>
                 <Text className={"text-sm" +textClass}>{item.name}</Text>
             </View>
